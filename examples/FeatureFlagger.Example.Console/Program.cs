3eb4fa0a7597c26b24b55f0f5e95cb5453e185ef
Richard Bogle <richard.bogle@unsettling.co.uk>
2017-11-19 08:07:13 +0000

    Code tidy

--- examples/FeatureFlagger.Example.Console/Program.cs
+++ examples/FeatureFlagger.Example.Console/Program.cs
@@ -1,7 +1,7 @@
-﻿using System;
-
-namespace FeatureFlagger.Example.Console
+﻿namespace FeatureFlagger.Example.Console
 {
+    using System;
+
     class Program
     {
         static void Main(string[] args)
