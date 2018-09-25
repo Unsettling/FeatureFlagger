004f76a1d0a39d22976e5c2202e115ef12fc744c
Richard Bogle <Boggin@users.noreply.github.com>
2018-09-25 15:18:26 +0100

    Create Constants file

---
+++ src/FeatureFlagger/Constants.cs
@@ -0,0 +1,15 @@
+namespace Unsettling.FeatureFlagger
+{
+    public static class Constants
+    {
+        public const string Config = "CONFIG";
+        public const string Date = "DATE";
+        public const string Enabled = "ENABLED";
+        public const string Feature = "FEATURE";
+        public const string Lookup = "LOOKUP";
+        public const string Name = "NAME";
+        public const string Store = "STORE";
+        public const string User = "USER";
+        public const string Users = "USERS";
+    }
+}
