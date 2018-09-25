257f1a5d307c02265c5d72b8fca1acdfa3b1f0aa
Richard Bogle <Boggin@users.noreply.github.com>
2018-09-25 15:05:12 +0100

    Removed FlagTypes

--- Flags.sql
+++ Flags.sql
@@ -3,11 +3,7 @@ CREATE TABLE [dbo].[Flags] (
         [Id] int IDENTITY(1,1) NOT NULL,
         [Name] nvarchar(max)  NOT NULL,
         [Description] nvarchar(max)  NOT NULL,
-        [LastUpdatedBy] nvarchar(50)  NOT NULL,
-        [LastUpdatedAt] datetime  NOT NULL,
-        [UpdateVersion] ROWVERSION  NULL,
-        [FeatureId] int  NOT NULL,
-        [FlagType_Id] int  NOT NULL
+        [FeatureId] int  NOT NULL
     );
 GO
 
@@ -31,19 +27,3 @@ CREATE INDEX [IX_FK_FeatureFlag]
 ON [dbo].[Flags]
     ([FeatureId]);
 GO
-
--- Creating foreign key on [FlagType_Id] in table 'Flags'
-ALTER TABLE [dbo].[Flags]
-ADD CONSTRAINT [FK_FlagToFlagType]
-    FOREIGN KEY ([FlagType_Id])
-    REFERENCES [dbo].[FlagTypes]
-    ([Id])
-    ON DELETE NO ACTION ON UPDATE NO ACTION;
-GO
-
--- Creating non-clustered index for FOREIGN KEY 'FK_FlagToFlagType'
-CREATE INDEX [IX_FK_FlagToFlagType]
-ON [dbo].[Flags]
-    ([FlagType_Id]);
-GO
-
