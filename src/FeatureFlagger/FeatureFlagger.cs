3efa45f9e15fd0082d36aa6c0505dc92e2df40aa
Richard Bogle <Boggin@users.noreply.github.com>
2018-09-25 15:25:06 +0100

    Extend FeatureFlagger class

--- src/FeatureFlagger/FeatureFlagger.cs
+++ src/FeatureFlagger/FeatureFlagger.cs
@@ -8,36 +8,66 @@
     using Behaviours;
     using ConfigurationReaders;
 
-    public class FeatureFlagger
+    public sealed class FeatureFlagger
     {
         static FeatureFlagger()
         {
-            // fancy footwork here helps to do the MEF composition
-            // without requiring the user to instantiate the class themselves.
-            // (thanks, Gary).
-            var ff = new FeatureFlagger();
+            // ReSharper disable once ObjectCreationAsStatement
+            new FeatureFlagger();
         }
-            
-        public FeatureFlagger()
-        {
-            // TODO: suspect this may be a problem for picking up new behaviours ...
-            var configuration =
-                new ContainerConfiguration()
-                .WithAssemblies(
-                    new[]
-                    {
-                        typeof(IBehaviour).GetTypeInfo().Assembly,
-                        typeof(IConfigurationReader).GetTypeInfo().Assembly
-                    });
 
-            var compositionHost = configuration.CreateContainer();
-            compositionHost.SatisfyImports(this);
+        private FeatureFlagger()
+        {
+            SetImports();
+            SetReader();
+            SetFeatures();
         }
 
         [ImportMany]
-        public static IEnumerable<IBehaviour> Behaviours { get; set; }
+        public static IEnumerable<IBehaviour> Behaviours { get; private set; }
 
-        [Import]
-        public static IConfigurationReader Reader { get; set; }
+        public static IEnumerable<Feature> Features { get; private set; }
+
+        public static IConfigurationReader Reader { get; private set; }
+
+        [ImportMany]
+        private static IEnumerable<IConfigurationReader> Readers { get; set; }
+
+        private static void SetFeatures()
+        {
+            Features = Reader.ReadAll();
+        }
+
+        private static void SetReader()
+        {
+            // set the configuation reader based on an appSetting.
+            var source =
+                ConfigurationManager.AppSettings["FeatureFlaggerSource"]
+                ?? Constants.Config;
+            Reader =
+                Readers.ToList()
+                    .Find(
+                        f =>
+                        f.Name.Equals(
+                            source,
+                            StringComparison.OrdinalIgnoreCase));
+        }
+
+        private void SetImports()
+        {
+            // wouldn't it be nice if Microsoft.Composition was available?
+            using (var catalog = new AggregateCatalog())
+            {
+                catalog.Catalogs.Add(
+                    new AssemblyCatalog(
+                        typeof(IBehaviour).GetTypeInfo().Assembly));
+                using (var container = new CompositionContainer(catalog))
+                {
+                    container.SatisfyImportsOnce(this);
+                    Behaviours = container.GetExportedValues<IBehaviour>();
+                    Readers = container.GetExportedValues<IConfigurationReader>();
+                }
+            }
+        }
     }
 }
