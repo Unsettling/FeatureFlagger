eed7cdfc53df346b99e143942d15037113c84b9c
Richard Bogle <Boggin@users.noreply.github.com>
2018-09-25 15:09:22 +0100

    Update IConfigurationReader

--- src/FeatureFlagger.ConfigurationReaders/IConfigurationReader.cs
+++ src/FeatureFlagger.ConfigurationReaders/IConfigurationReader.cs
@@ -1,17 +1,30 @@
 ﻿namespace FeatureFlagger.ConfigurationReaders
 {
+    using System.Collections.Generic;
+
     using FeatureFlagger.Domain;
 
     public interface IConfigurationReader
     {
         /// <summary>
+        /// The Name of the implementation.
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// Read a feature from the configuration.
+        /// </summary>
+        /// <param name="featureName">The name of the feature.</param>
+        /// <returns>A Feature.</returns>
+        Feature Read(string featureName);
+
+        /// <summary>
         /// Read the configuration.
         /// </summary>
-        /// <param name="name">The name of the feature flag.</param>
         /// <returns>
         /// A dictionary where the key is a property name and the values are
         /// the property's values ordered alphabetically by attribute name.
         /// </returns>
-        Feature Read(string name);
+        IEnumerable<Feature> ReadAll();
     }
 }
