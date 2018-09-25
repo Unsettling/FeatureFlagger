aa26720c107ec3a184293ef45ce542a27d4312b3
Richard Bogle <Boggin@users.noreply.github.com>
2018-09-25 15:13:23 +0100

    Create StoreReader.cs

---
+++ src/FeatureFlagger.ConfigurationReaders/StoreReader.cs
@@ -0,0 +1,89 @@
+namespace Unsettling.FeatureFlagger.ConfigurationReaders
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.Composition;
+    using System.Configuration;
+    using System.Data.SqlClient;
+    using System.Linq;
+
+    using Feature = Unsettling.FeatureFlagger.Feature;
+
+    [Export(typeof(IConfigurationReader))]
+    public class StoreReader : IConfigurationReader
+    {
+        private StoreReader()
+        {
+            Name = "STORE";
+        }
+
+        public string Name { get; }
+
+        public Feature Read(string featureName)
+        {
+            return
+                FeatureFlagger.Features.ToList()
+                    .Find(
+                        f =>
+                        f.Name.Equals(
+                            featureName,
+                            StringComparison.OrdinalIgnoreCase));
+        }
+
+        public IEnumerable<Feature> ReadAll()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["IntermediaryManagement_DB"].ConnectionString;
+
+            const string QueryString =
+                "SELECT Features.Name, Flags.Name, FlagProperties.PropertyKey, FlagProperties.PropertyValue "
+               + "FROM Features "
+               + "JOIN Flags ON Features.Id = Flags.FeatureId "
+               + "JOIN FlagProperties ON Flags.Id = FlagProperties.FlagId "
+               + "ORDER BY Features.Name, Flags.Name, FlagProperties.PropertyKey";
+
+            var features = new List<Feature>();
+
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand(QueryString, connection))
+            {
+                connection.Open();
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    var featureName = reader.GetString(0).Trim().ToUpperInvariant();
+                    var flagName = reader.GetString(1).Trim().ToUpperInvariant();
+                    var propertyKey = reader.GetString(2).Trim().ToUpperInvariant();
+                    var propertyValue = reader.GetString(3).Trim().ToUpperInvariant();
+
+                    Feature feature;
+                    if (!features.Exists(l => l.Name.Equals(featureName)))
+                    {
+                        feature = new Feature(featureName);
+                        features.Add(feature);
+                    }
+                    else
+                    {
+                        feature = features.First(l => l.Name.Equals(featureName));
+                    }
+
+                    Flag flag;
+                    if (!feature.Flags.Exists(l => l.Name.Equals(flagName)))
+                    {
+                        flag = new Flag(flagName);
+                        feature.Flags.Add(flag);
+                    }
+                    else
+                    {
+                        flag = feature.Flags.First(l => l.Name.Equals(flagName));
+                    }
+
+                    flag.Properties.Add(propertyKey, propertyValue);
+                }
+
+                reader.Close();
+            }
+
+            return features;
+        }
+    }
+}
