d87f651f486a976f998f03c09769188151f43db6
Richard Bogle <Boggin@users.noreply.github.com>
2018-09-25 15:02:15 +0100

    Remove audit fields

--- Features.sql
+++ Features.sql
@@ -2,10 +2,7 @@
 CREATE TABLE [dbo].[Features] (
     [Id] int IDENTITY(1,1) NOT NULL,
     [Name] nvarchar(max)  NOT NULL,
-    [Description] nvarchar(max)  NOT NULL,
-    [LastUpdatedBy] nvarchar(50)  NOT NULL,
-    [LastUpdatedAt] datetime  NOT NULL,
-    [UpdateVersion] binary(8)  NULL
+    [Description] nvarchar(max)  NOT NULL
 );
 GO
 -- Creating primary key on [Id] in table 'Features'
